fbe08bc8b6dfb1b7f6d2492b0490b64f79578b0e
Saqib Ahmed <115645938+SaqibatCode@users.noreply.github.com>
2024-04-28 20:23:49 +0000

    t2

--- basics/index.js
+++ basics/index.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+const accountId = 12414124;
+let accountEmail = "hello@gmail.com";
+var accountPassword = "12345";
+accountCity = "Karachi"
+let accountState;
+// accountId = 2;
+
+accountEmail = "Hello world";
+accountPassword = "Sindh";
+accountCity = "Hyderabel"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
